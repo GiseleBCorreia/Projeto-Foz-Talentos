c0a76079a44d8f07b6c5466060e28a8d8de9643f
Caio Govea <contato.caiogovea@gmail.com>
2026-08-05 18:54:35 -0300

    fix: http request

--- foz-talentos-api/src/main/java/br/com/foztalentos/api/config/OpenApiConfig.java
+++ foz-talentos-api/src/main/java/br/com/foztalentos/api/config/OpenApiConfig.java
@@ -1,14 +1,17 @@
 package br.com.foztalentos.api.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -19,25 +22,37 @@ public class OpenApiConfig {
 
         final String securitySchemeName = "Bearer Authentication";
 
-        return new OpenAPI().info(new Info()
-                        .title("Foz Talentos API")
-                        .version("1.0")
-                        .description("""
-                                API responsável pelo gerenciamento de:
-                                - Administradores
-                                - Categorias
-                                - Vagas
-                                - Autenticação JWT
-                                """)
-                    .contact(new Contact().name("Equipe Foz Talentos").email("contato@foztalentos.com"))
-                    .license(new License().name("MIT")))
-                    .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)).components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        ));
+        return new OpenAPI()
+
+                // Servidor de produção (Railway)
+                .servers(List.of(new Server()
+                                .url("https://foz-talentos-api-production.up.railway.app")
+                                .description("Production Server")))
+
+                .info(new Info()
+                                .title("Foz Talentos API")
+                                .version("1.0")
+                                .description("""
+                                        API responsável pelo gerenciamento de:
+                                        - Administradores
+                                        - Categorias
+                                        - Vagas
+                                        - Autenticação JWT
+                                        """)
+                                .contact(new Contact()
+                                        .name("Equipe Foz Talentos")
+                                        .email("contato@foztalentos.com"))
+                                .license(new License().name("MIT")))
+
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+
+                .components(new Components().addSecuritySchemes(securitySchemeName,
+                            new SecurityScheme()
+                                    .name(securitySchemeName)
+                                    .type(SecurityScheme.Type.HTTP)
+                                    .scheme("bearer")
+                                    .bearerFormat("JWT")
+                                )
+                );
     }
 }
